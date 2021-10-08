6c0c7519b1ed2d6b05961d78dc7deec2aeb9d012
Giugurtah <c.buoncompagni.98@gmail.com>
2021-10-08 16:38:50 +0200

    Creazione database

---
+++ database/migrations/2021_10_08_141604_update_travels_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->text("description");
+            $table->tinyInteger("duration");
+            $table->foreignId('transport_id');
+        });
+
+        Schema::create('transport', function (Blueprint $table) {
+            $table->id();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            //
+        });
+
+        Schema::dropIfExists('transport');
+    }
+}
